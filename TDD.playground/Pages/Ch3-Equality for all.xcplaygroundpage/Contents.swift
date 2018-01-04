0ebbb38601a3541daaf34d3629c666d6534b0f1a
Bogdan Raducan <bogdan.raducan@thinslices.com>
2018-01-04 20:15:34 +0200

    Add test for equality. Compiler fails.

---
+++ TDD.playground/Pages/Ch3-Equality for all.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+import Foundation
+import XCTest
+
+class Dollar {
+    var amount: Int
+
+    init(_ amount: Int) {
+        self.amount = amount
+    }
+
+    func times(_ by: Int) -> Dollar {
+        return Dollar(self.amount * by)
+    }
+}
+
+class DollarTests: XCTestCase {
+    func testMultiplication() {
+        let five = Dollar(5)
+        var product = five.times(2)
+        XCTAssertEqual(5 * 2, product.amount)
+        product = five.times(3)
+        XCTAssertEqual(5 * 3, product.amount)
+    }
+
+    func testEquality() {
+        XCTAssertEqual(Dollar(5), Dollar(5))
+    }
+}
+
+// Running the tests
+class TestObserver: NSObject, XCTestObservation {
+    func testCase(_ testCase: XCTestCase,
+                  didFailWithDescription description: String,
+                  inFile filePath: String?,
+                  atLine lineNumber: Int) {
+        assertionFailure(description, line: UInt(lineNumber))
+    }
+}
+
+let testObserver = TestObserver()
+XCTestObservationCenter.shared.addTestObserver(testObserver)
+DollarTests.defaultTestSuite.run()
+
