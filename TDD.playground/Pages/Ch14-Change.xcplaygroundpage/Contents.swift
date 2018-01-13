9359e38b64ccd0eabc00566bbfc4ae186a055254
Bogdan Raducan <bogdan.raducan@thinslices.com>
2018-01-13 10:41:28 +0200

    Update initial backlog

--- TDD.playground/Pages/Ch14-Change.xcplaygroundpage/Contents.swift
+++ TDD.playground/Pages/Ch14-Change.xcplaygroundpage/Contents.swift
@@ -1,11 +1,10 @@
 /*:
  * $5 + 10 CHF = $10 if rate is 2:1
- * **$5 + $5 = $10**
+ * $5 + $5 = $10
  * Return Money from $5 + $5
- * Money rounding?
- * hashCode()
- * Equal null
- * Equal object
+ * OK - Bank.reduce(Money)
+ * **Reduce Money with conversion**
+ * Reduce(Bank, String)
  */
 
 import Foundation
