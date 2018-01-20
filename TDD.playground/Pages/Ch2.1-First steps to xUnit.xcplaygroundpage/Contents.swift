2caf7da4269446c0c5f81c0877e742a5756ce654
Bogdan Raducan <bogdan.raducan@thinslices.com>
2018-01-20 08:44:26 +0200

    End chpt with updated backlog and learnings

--- TDD.playground/Pages/Ch2.1-First steps to xUnit.xcplaygroundpage/Contents.swift
+++ TDD.playground/Pages/Ch2.1-First steps to xUnit.xcplaygroundpage/Contents.swift
@@ -46,3 +46,20 @@ class TestCaseTest: TestCase {
 
 TestCaseTest(#selector(TestCaseTest.testCaseRunnin)).run()
 
+/* Backlog:
+ * OK - Invoke test method
+ * Invoke setUp first
+ * Invoke tearDown afterward
+ * Invoke tearDown even if the test method fails
+ * Run multiple tests
+ * Report collected results
+ */
+
+/*:
+ Learnings:
+ * After a couple of hubris-fueled false starts, figured out how to begin with a tiny little step
+ * Implemented functionality, by first hardwiring it, and then making it more general by replacing constants with variables
+ * Used Pluggable Selector, which we promise not to use again for four months, minimum, because it makes code hard to analyze statically
+ * Bootstrapped our testing framework, all in tiny steps
+ */
+
