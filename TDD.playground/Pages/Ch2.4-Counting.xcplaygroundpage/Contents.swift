b603846013239846a485793974f615ddd1542675
Bogdan Raducan <bogdan.raducan@thinslices.com>
2018-01-28 08:46:17 +0200

    Ch 2.4 initial commit

---
+++ TDD.playground/Pages/Ch2.4-Counting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,73 @@
+import Foundation
+import XCTest
+
+/* Backlog:
+ * OK - Invoke test method
+ * OK - Invoke setUp first
+ * OK - Invoke tearDown afterward
+ * Invoke tearDown even if the test method fails
+ * Run multiple tests
+ * **Report collected results**
+ * OK - Log string in WasRun
+ */
+
+class TestCase: NSObject {
+//    let testMethodSelector: Selector
+
+    init(_ testMethodSelector: Selector?) {
+//        self.testMethodSelector = testMethodSelector
+    }
+
+    func setup() { }
+
+    func run() { }
+
+    func tearDown() { }
+
+    func testMethod() { }
+    func testTemplateMethod() { }
+}
+
+class WasRun: TestCase {
+    var log = String()
+
+    override init(_ testMethodSelector: Selector?) {
+        super.init(testMethodSelector)
+    }
+
+    override func run() {
+        self.setup()
+        self.testMethod()
+        self.tearDown()
+    }
+
+    override func setup() {
+        self.log = "setup"
+    }
+
+    override func testMethod() {
+        self.log += " testMethod"
+    }
+
+    override func tearDown() {
+        self.log += " tearDown"
+    }
+
+    override func testTemplateMethod() { }
+}
+
+class TestCaseTest: TestCase {
+    override func run() {
+        self.setup()
+        self.testTemplateMethod()
+    }
+
+    override func testTemplateMethod() {
+        let test = WasRun(nil)
+        test.run()
+        XCTAssertTrue(test.log == "setup testMethod tearDown")
+    }
+}
+
+let a = TestCaseTest(nil)
+a.run()
